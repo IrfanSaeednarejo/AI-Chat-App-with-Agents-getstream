5a42c3f78f71c62a2dce601cdc9e99f00bc94456
Irfan Saeed <irfansaeed.narejo@gmail.com>
2026-01-12 22:23:03 +0500

    feat: enforce environment variable checks and type assertions in serverClient.ts

--- Backend-ai-assistant/src/serverCLient.ts
+++ Backend-ai-assistant/src/serverCLient.ts
@@ -1,7 +1,12 @@
 import {streamChat} from 'stream-chat';
 
 
-export const apiKey = process.env.STREAM_API_KEY;
-export const apiSecret = process.env.STREAM_API_SECRET;
+export const apiKey = process.env.STREAM_API_KEY as string;
+export const apiSecret = process.env.STREAM_API_SECRET as string;
 
 
+if(!apiKey || !apiSecret) {
+  throw new Error('Environment Variables are missing, STREAM_API_KEY and STREAM_API_SECRET must be set');
+}
+
+export const serverClient = new streamChat(apiKey, apiSecret);
